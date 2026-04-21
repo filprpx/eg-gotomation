45baf670c3730f7e75b430aea2ff79661aca4683
filprpx <filipe.uccelli@gmail.com>
2026-04-21 10:41:29 -0300

    Make client code consume from internal netbox logic. (Working!)

--- cmd/rundeck/netbox/retrieve_data.go
+++ cmd/rundeck/netbox/retrieve_data.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/filprpx/eg-gotomation/internal/netbox"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	netbox, err := netbox.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	devices, err := netbox.DCIM.Device.List(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Retrieved %d devices from netbox:\n", len(devices))
+	for i, device := range devices {
+		fmt.Printf("%d. %s\n", i+1, device.Name)
+	}
 }
